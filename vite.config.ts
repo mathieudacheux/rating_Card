d0b92e1d378904d8d4e68e41e0e666ce18915d51
Mathieu Dacheux <twinklose@gmail.com>
2024-08-16 20:16:02 +0200

    [main] deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  base: '',
+  plugins: [react(), viteTsconfigPaths()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: 'main.js',
+        assetFileNames: '[name].[ext]',
+        chunkFileNames: '[name].js',
+      },
+      preserveEntrySignatures: 'strict',
+    },
+    minify: true,
+    modulePreload: {
+      polyfill: false,
     },
   },
-});
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+})
